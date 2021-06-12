c9a943deca883ca4d7bacebfc74fcf0f6c55a653
Kristina Boratkova <kboratkova@LAPTOP-0DL33T0C.localdomain>
2021-06-12 15:28:09 +0200

    hotovo

---
+++ http_get.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+import re
+import os
+import ssl
+
+url = sys.argv[1]
+
+isOkay=0
+while isOkay == 0:
+    headers = {}
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  
+    urlComp = re.match('(?:([^:\\/?#]+):)?(?:\\/\\/([^\\/?#]*))?([^?#]*)(?:\\?([^#]*))?(?:#(.*))?',url)
+
+    path = urlComp.group(0).strip(urlComp.group(1))
+    path = path.strip('://')
+    path = path.strip(urlComp.group(2))
+    path = path.strip('/')
+    is_ssl = urlComp.group(1)
+
+
+    if is_ssl == 'https':
+        hostname = urlComp.group(2)
+        s.connect((hostname,443))
+        s=ssl.wrap_socket(s)
+    else:
+        hostname = urlComp.group(2)
+        s.connect((hostname,80))
+
+
+    request_header = 'GET /' + path + ' HTTP/1.1\r\nHost:' + hostname + '\r\nAccept-charset: UTF-8\r\n\r\n'
+    f=s.makefile('rwb')
+    f.write(request_header.encode('ASCII'))
+    f.flush()
+
+    status=f.readline()    
+    status.decode('ASCII')
+    version,statNum,comment=status.split(maxsplit=2)
+    if int(statNum) == 200:
+        isOkay =  200
+        while True:
+            header=f.readline()
+            header.decode('ASCII')
+            if ':'.encode('ASCII') in header:
+                hlavicka,obsah=header.split(':'.encode('ASCII'),1)
+                hlavicka = hlavicka.lower()
+                obsah=obsah.lower()
+                headers[hlavicka]=obsah
+            else:
+                break
+        for key,value in headers.items():
+            if 'content-length'.encode('ASCII') in key:
+                data = f.read(int(value))
+                sys.stdout.buffer.write(data)
+            else:
+                if 'transfer-encoding'.encode('ASCII') in key and ' chunked'.encode('ASCII') in value:
+                    blockSize = 1
+                    while blockSize > 0:
+                        numBytes=f.readline()
+                        blockSize=int(numBytes,16)
+                        data=f.read(blockSize)
+                        sys.stdout.buffer.write(data)
+                        f.readline()
+
+    else:
+        if int(statNum) in [301,302,303,307,308]:
+            while True:
+                header=f.readline()
+                header.decode('ASCII')
+                if ':'.encode('ASCII') in header:
+                    hlavicka,obsah=header.split(':'.encode('ASCII'),1)
+                    hlavicka = hlavicka.lower()
+                    obsah=obsah.lower()
+                    headers[hlavicka]=obsah
+                else:
+                    break
+            for key,value in headers.items():
+                if 'location'.encode('ASCII') in key:
+                    url = value.decode('UTF-8')
+                    url = url.strip()
+                    del f
+                    del s
+                    del headers
+                    del status
+                    continue
+        else:
+            sys.stderr.write('Unknown request/Bad request')
+            sys.exit(1)
+
+f.close()   
+s.close()  
+
+
+
+
